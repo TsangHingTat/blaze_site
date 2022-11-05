988994e2c7e260b1f1191c6af55378ba1f13b087
TsangHingTat <hingtattsang@gmail.com>
2022-11-06 01:42:36 +0800

    update

---
+++ PLC/PLCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLCApp.swift
+//  PLC
+//
+//  Created by HingTatTsang on 5/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PLCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
